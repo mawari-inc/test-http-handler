2f134eb10b7d3425b363e35c2ffba091fb2ac40e
Rodrigo Gutierrez <r.gutierrez@dverse.me>
2018-06-19 16:50:54 +0900

    expand to handle several request types

--- adServerHTTP.js
+++ adServerHTTP.js
@@ -3,14 +3,61 @@
 const http = require("http");
 const port = 8888;
 
-const server = http.createServer((request, response) =>
+function handleGetRequest(response)
 {
     response.writeHead(200, { "Content-Type" : "text/plain" });
+    response.end("GET action was requested.");
+}
 
-    response.end("Hello from HTTP Ad Server!");
+function handlePostRequest(response)
+{
+    response.writeHead(200, { "Content-Type" : "text/plain" });
+    response.end("POST action was requested.");
+}
 
-    console.log("Handler requested...");
-});
+function handlePutRequest(response)
+{
+    response.writeHead(200, { "Content-Type" : "text/plain" });
+    response.end("PUT action was requested.");
+}
+
+function handleDeleteRequest(response)
+{
+    response.writeHead(200, { "Content-Type" : "text/plain" });
+    response.end("DELETE action was requested.");
+}
+
+function handleBadRequest(response)
+{
+    console.log("Unsupported HTTP method requested.\n")
+    response.writeHead(400, { "Content-Type" : "text/plain" });
+    response.end("Bad request.");
+}
+
+function handleRequest(request, response)
+{
+    switch(request.method)
+    {
+        case "GET":
+        handleGetRequest(response);
+        break;
+        case "POST":
+        handlePostRequest(response);
+        break;
+        case "PUT":
+        handlePutRequest(response);
+        break;
+        case "DELETE":
+        handleDeleteRequest(response);
+        break;
+        default:
+        handleBadRequest(response);
+        break;
+    }
+    console.log("Request processing complete.\n");
+}
+
+const server = http.createServer(handleRequest);
 
 server.listen(port, "localhost", () =>
 {
