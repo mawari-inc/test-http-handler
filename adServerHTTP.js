cd6aa89d0df3115fe7b330b6810ecf7af7fcbabb
Rodrigo Gutierrez <r.gutierrez@dverse.me>
2018-06-20 19:19:04 +0900

    fix port

--- adServerHTTP.js
+++ adServerHTTP.js
@@ -1,13 +1,13 @@
 // content of adServerHTTP.js
 
 const http = require("http");
-const port = 8888;
+const port = process.env.PORT || 8888;
 
 const requestModule = require("./requestModule");
 
 const server = http.createServer(requestModule.handleRequest);
 
-server.listen(port, "localhost", () =>
+server.listen(port, () =>
 {
-    console.log(`Started Node.js server, listening on port ${port}.`);
+    console.log(`Started Rodrigo's Node.js server.\nListening on port ${port}.\n`);
 });
