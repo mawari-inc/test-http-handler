cb16bfbfaea79de2ada35ebec96061ec6e7339ce
Rodrigo Gutierrez <r.gutierrez@dverse.me>
2018-06-20 16:33:27 +0900

    extract http handler code into separate file

---
+++ requestModule.js
@@ -0,0 +1,53 @@
+function handleGetRequest(response)
+{
+    response.writeHead(200, { "Content-Type" : "text/plain" });
+    response.end("GET action was requested.");
+}
+
+function handlePostRequest(response)
+{
+    response.writeHead(200, { "Content-Type" : "text/plain" });
+    response.end("POST action was requested.");
+}
+
+function handlePutRequest(response)
+{
+    response.writeHead(200, { "Content-Type" : "text/plain" });
+    response.end("PUT action was requested.");
+}
+
+function handleDeleteRequest(response)
+{
+    response.writeHead(200, { "Content-Type" : "text/plain" });
+    response.end("DELETE action was requested.");
+}
+
+function handleBadRequest(response)
+{
+    console.log("Unsupported HTTP method requested.\n")
+    response.writeHead(400, { "Content-Type" : "text/plain" });
+    response.end("Bad request.");
+}
+
+exports.handleRequest = function (request, response)
+{
+    switch(request.method)
+    {
+        case "GET":
+        handleGetRequest(response);
+        break;
+        case "POST":
+        handlePostRequest(response);
+        break;
+        case "PUT":
+        handlePutRequest(response);
+        break;
+        case "DELETE":
+        handleDeleteRequest(response);
+        break;
+        default:
+        handleBadRequest(response);
+        break;
+    }
+    console.log("Request processing complete.\n");
+}
